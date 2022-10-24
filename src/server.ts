2d2e2aebe8f446f79d2867ba12735d29b6c7c235
kauawb02 <kaua.wadson@aluno.senai.br>
2022-10-24 08:37:06 -0300

    Atualizando erro cors | estudar a documentação da biblioteca cros.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import colors from 'colors';
-import cors from 'cors';
 import routes from './routes/routeControllers';
 
 const app: express.Application = express();
 
+const cors = require('./config/cors')
+
+// https://www.section.io/engineering-education/how-to-use-cors-in-nodejs-with-express/ 
+// Documentação para estudar pacero
 app.use(express.json());
 app.use(cors)
 app.use("/agenda", routes);
