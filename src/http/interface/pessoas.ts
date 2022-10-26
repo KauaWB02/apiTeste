03d050c396eae43d0b36a9b9427025d4724192a3
kauawb02 <kaua.wadson@aluno.senai.br>
2022-10-26 15:57:21 -0300

    ?

--- src/http/interface/pessoas.ts
+++ src/http/interface/pessoas.ts
@@ -1,13 +1,13 @@
 export default interface IPERSONS {
-    nome: String,
-    sobreNome: String,
-    email?: String,
-    senha?: String,
-    idade?: Number,
-    telefone: Number,
-    pais?: String,
-    sexo?: String,
-    altura?: Number,
-    peso?: Number,
-    descricao?: String
+    nome?: String,
+    sobreNome?: String,
+    email: String,
+    senha: String,
+    idade: Number,
+    telefone?: Number,
+    pais: String,
+    sexo: String,
+    altura: Number,
+    peso: Number,
+    descricao: String
 };
